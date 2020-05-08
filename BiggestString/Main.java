07c0cc3294d8aca9c44ccc4dbdd8efa80f0dce42
Blake Stevenson <24233354+BlakeStevenson@users.noreply.github.com>
2020-05-08 17:08:02 -0500

    Create Main.java

---
+++ BiggestString/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        biggestString run = new biggestString();
+        String[] strings = {"red", "blue", "green", "yellow"};
+        System.out.println(run.getBiggest(strings));
+    }
+}
