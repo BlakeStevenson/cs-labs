5c986669a89575543c01f3c3a09942ccb5119c20
Blake Stevenson <24233354+BlakeStevenson@users.noreply.github.com>
2020-05-08 17:11:52 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,114 @@
+import java.util.Scanner;
+public class main {
+   public static void main(String args[]) {
+       System.out.println("2. Hello World!");
+       // print hello world
+       System.out.println("Hello World!");
+       // separate sections
+       System.out.println("\n3. Hello World");
+      // print hello
+       System.out.println("Hello");
+       // print world
+       System.out.println("World!");
+       // print YOUR JAVA CODE FOR THIS PROBLEM HERE.
+       System.out.println("YOUR JAVA CODE FOR THIS PROBLEM HERE.");
+       // separate sections
+       System.out.println("\n4. Hello World - 2 line breaks");
+       /* print
+       Hello
+
+
+       World */
+       System.out.println("Hello\n\nWorld");
+       // separate sections
+       System.out.println("\n5. Hello World with *'s");
+       // print
+       // *************
+       //*** Hello ***
+       //*** World ***
+       //*************
+       System.out.println("*************");
+       System.out.println("*** Hello ***");
+       System.out.println("*** World ***");
+       System.out.println("*************");
+       // separate sections
+       System.out.println("\n6. Hello World 2 lines");
+       // hello world print/println one line
+       System.out.print("Hello ");
+       System.out.println("World!");
+       // separate sections
+       System.out.println("\n7. Hashtags");
+       // ##'s
+       System.out.println("###");
+       System.out.println("# #");
+       System.out.println("###");
+       // separate sections
+       System.out.println("\n8. First Name x3");
+       // name using println/print
+       System.out.print("Blake, Blake");
+       System.out.println("\n");
+       System.out.println("\n");
+       System.out.println("Blake");
+       // separate sections
+       System.out.println("\n9. Something");
+       // Something Something SOMETHING
+       System.out.print("Something ");
+       System.out.print("Something ");
+       System.out.println("SOMETHING");
+       // separate sections
+       System.out.println("\n10. Java is Cool");
+       // java is cool
+       System.out.println("Java is\ncool\n!");
+       // separate sections
+       System.out.println("\n11. First Initial");
+       // first inital ascii art
+       System.out.println("******");
+       System.out.println("*     *");
+       System.out.println("*    *");
+       System.out.println("******");
+       System.out.println("*     *");
+       System.out.println("*    *");
+       System.out.println("*   *");
+       System.out.println("***");
+       // separate sections
+       System.out.println("\n12. Riddle");
+       // riddle
+       Scanner name = new Scanner(System.in);
+       System.out.println("Mary’s father has five daughters – Nana, Nene, Nini, Nono. What is the fifth daughter’s name?");
+       if(name.nextLine().equals("Mary")) {
+           System.out.println("You got it right! The daughter's name is Mary.");
+       } else {
+           System.out.println("You got it wrong! The daughter's name was Mary.");
+       }
+       // separate sections
+       System.out.println("\n13. Custom ASCII Art");
+       // ascii art
+       System.out.println("___________________________     |‾‾‾‾‾‾‾‾‾‾‾‾‾|");
+       System.out.println("|                                                    |     |     DeLL    |");
+       System.out.println("|                                                    |     |_____________|");
+       System.out.println("|          JAVA                                  |     | |‾|‾|   |‾| |");
+       System.out.println("|           IS                                      |     | |_|_|   | | |");
+       System.out.println("|         AMAZING                           |     | |‾|‾|   | | |");
+       System.out.println("|            !                                       |     | |_|_|   | | |");
+       System.out.println("|__________________________|     |  ° °    |_| |");
+       System.out.println("                              |   |                |_____________|");
+       System.out.println("         ‾‾‾‾‾‾‾‾‾              |_____________|");
+       System.out.println("|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|");
+       System.out.println("|                                                  |");
+       System.out.println("|                                                  |");
+       System.out.println("|                                                  |");
+       System.out.println("|                                                  |");
+       System.out.println("|                                                  |");
+       System.out.println("|                                                  |");
+       System.out.println("|                                                  |");
+       System.out.println("|                                                  |");
+       //separate sections
+       System.out.println("\n14. Advanced ASCII Art");
+       System.out.println("**********");
+       System.out.println("* \\\\\\/// *");
+       System.out.println("*  \\\\//  *");
+       System.out.println("*   \\/   *");
+       System.out.println("**********");
+
+   }
+}
